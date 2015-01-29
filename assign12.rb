e48c588621bb0836b742de053f131d3216bf4a1a
John C Cato <JohnCCato@Johns-MacBook-Pro.local>
2015-01-29 15:15:15 -0500

    assign12

---
+++ assign12.rb
@@ -0,0 +1,47 @@
+
+
+assign = ["What is your name?","How old are you?","Where do you live?"]
+
+assign.each do |userq|
+  puts userq
+  user = gets.chomp
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#assign.each(a) = gets.chomp
+
+#{|x| print x, "--"}
+
+
+
+
+
+
+
+
+
+ #puts assign.each {}
