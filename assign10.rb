75c08ca01f7a5a78e32e3f5a1b04dbcf9015613d
John C Cato <JohnCCato@Johns-MacBook-Pro.local>
2015-01-29 18:48:37 -0500

    assignment10

---
+++ assign10.rb
@@ -0,0 +1,12 @@
+#WORKING WITH STRINGS
+puts "Hello, how are you?"
+
+strings = gets.chomp.strip
+
+puts strings.reverse
+
+puts strings.reverse.chop
+
+puts strings.reverse.chop.reverse
+
+print strings.split(/ /).count
