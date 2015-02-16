fd87e636572ab0196ef2be12d1477e544bea39f5
John C Cato <hga813@gmail.com>
2015-02-16 14:39:14 -0500

    Assign.29

---
+++ movie_query/app/controllers/movies_controller.rb
@@ -0,0 +1,31 @@
+
+#puts "#{movie['title']}:#{movie['synopsis']} \n\n"
+#  puts 'Movie: ', movie.values_at('title').join(' '), "\n"
+#  puts'Synopsis: ', movie.values_at('synopsis').join(' '), "\n"
+
+class MoviesController < ApplicationController
+
+QUERY_URL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=g6erjnt3dwhzmjkyxt9sjvzt'
+
+  def search
+
+
+  movietitle = params['title']
+
+
+  uri = URI(QUERY_URL + "&q=#{movietitle}")
+
+  movie_json = Net::HTTP.get(uri)
+
+  parsed_info = JSON.parse(movie_json)['movies']
+
+  @movie = parsed_info.first
+
+  @synopsis = @movie.values_at('synopsis').join(' ')
+
+
+
+
+
+  end
+end
