b2b50e38bb6af98fb3f17e67aee176d822845c7b
John C Cato <hga813@gmail.com>
2015-02-17 10:11:34 -0500

    update

--- movie_query/app/controllers/movies_controller.rb
+++ movie_query/app/controllers/movies_controller.rb
@@ -17,8 +17,20 @@ QUERY_URL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=g6
 
   parsed_info = JSON.parse(movie_json)['movies']
 
-  @movie = parsed_info.first
+  movie = parsed_info.first
 
-  @synopsis = @movie.values_at('synopsis').join(' ')
+  #synopsis = @movie.values_at('synopsis').join(' ')
+
+  @title = movie['title']
+  @synopsis = movie['synopsis']
+  @year = movie['year']
+  @runtime = movie['runtime']
+  @mpaa_rating = movie['mpaa_rating']
+  @id = movie['id']
+  @posters = movie['posters']['original']
+  @poster = @posters.gsub('_tmb.jpg', '_ori.jpg')
+  @clips_link = movie['links']['clips']
+
+
   end
 end
