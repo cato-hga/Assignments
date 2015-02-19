f98c9000813763dd0ec8f793731b831290d721aa
John C Cato <hga813@gmail.com>
2015-02-19 17:36:25 -0500

    update

--- movie_query/app/controllers/movies_controller.rb
+++ movie_query/app/controllers/movies_controller.rb
@@ -1,8 +1,3 @@
-
-#puts "#{movie['title']}:#{movie['synopsis']} \n\n"
-#  puts 'Movie: ', movie.values_at('title').join(' '), "\n"
-#  puts'Synopsis: ', movie.values_at('synopsis').join(' '), "\n"
-
 class MoviesController < ApplicationController
 
 QUERY_URL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=g6erjnt3dwhzmjkyxt9sjvzt'
